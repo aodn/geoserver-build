23884e0bc9d6c7bcadc4ef684ed3ed798b60eb56
Jon Burgess <jkburges@gmail.com>
2014-05-21 16:28:49 +1000

    Don't bother calling the super implementation - just send error.

--- src/main/java/org/geoserver/ows/DispatcherWithHttpStatus.java
+++ src/main/java/org/geoserver/ows/DispatcherWithHttpStatus.java
@@ -1,5 +1,9 @@
 package org.geoserver.ows;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.geoserver.platform.Service;
@@ -7,8 +11,17 @@ import org.geoserver.platform.ServiceException;
 
 public class DispatcherWithHttpStatus extends org.geoserver.ows.Dispatcher {
 
+    /**
+     * Logging instance
+     */
+    static Logger logger = org.geotools.util.logging.Logging.getLogger("org.geoserver.ows");
+
     void handleServiceException(ServiceException se, Service service, Request request) {
-        super.handleServiceException(se, service, request);
-        request.getHttpResponse().setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        try {
+            request.getHttpResponse().sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        catch (IOException e) {
+            logger.log(Level.INFO, "Problem sending error", e);
+        }
     }
 }
