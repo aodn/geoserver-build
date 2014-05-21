9532e07489832c1d470c5d39e8babb52285229fe
Jon Burgess <jkburges@gmail.com>
2014-05-21 15:24:40 +1000

    Using constant for status code.

--- src/main/java/org/geoserver/ows/DispatcherWithHttpStatus.java
+++ src/main/java/org/geoserver/ows/DispatcherWithHttpStatus.java
@@ -1,5 +1,7 @@
 package org.geoserver.ows;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.geoserver.platform.Service;
 import org.geoserver.platform.ServiceException;
 
@@ -7,6 +9,6 @@ public class DispatcherWithHttpStatus extends org.geoserver.ows.Dispatcher {
 
     void handleServiceException(ServiceException se, Service service, Request request) {
         super.handleServiceException(se, service, request);
-        request.getHttpResponse().setStatus(500);
+        request.getHttpResponse().setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
 }
