4471504605769b0881a3fe4350b48c67caa0eadc
Tommy Fotak <tommy.fotak@gmail.com>
2014-12-24 08:30:01 +1100

    Remove unused methods and classes

--- src/main/java/au/org/emii/geoserver/extensions/filters/LayerFiltersService.java
+++ src/main/java/au/org/emii/geoserver/extensions/filters/LayerFiltersService.java
@@ -11,17 +11,9 @@ import au.org.emii.geoserver.extensions.filters.layer.data.DataDirectory;
 import au.org.emii.geoserver.extensions.filters.layer.data.Filter;
 import au.org.emii.geoserver.extensions.filters.layer.data.FiltersDocument;
 import au.org.emii.geoserver.extensions.filters.layer.data.io.FilterConfigurationFile;
-import au.org.emii.geoserver.extensions.filters.layer.data.io.LayerDataStore;
 import au.org.emii.geoserver.extensions.filters.layer.data.io.PossibleValuesReader;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
 import org.geoserver.catalog.Catalog;
-import org.geoserver.catalog.FeatureTypeInfo;
 import org.geoserver.catalog.LayerInfo;
-import org.geoserver.catalog.StoreInfo;
-import org.geotools.data.FeatureSource;
-import org.geotools.feature.FeatureIterator;
-import org.opengis.feature.Feature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -31,7 +23,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -39,7 +30,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
 
 public class LayerFiltersService {
 
@@ -91,33 +82,11 @@ public class LayerFiltersService {
         return new FiltersDocument().build(filters);
     }
 
-
-
-    private StoreInfo getStoreInfo(LayerInfo layerInfo) {
-        return layerInfo.getResource().getStore();
-    }
-
-    private String getStoreName(LayerInfo layerInfo) {
-        return getStoreInfo(layerInfo).getName();
-    }
-
     private LayerInfo getLayerInfo(String workspace, String layer) {
         return LayerInfoProperties.getLayer(getCatalog(), workspace, layer);
     }
 
     private String getLayerDataDirectoryPath(LayerInfo layerInfo) {
         return new DataDirectory(context).getLayerDataDirectoryPath(layerInfo);
-    }
-
-    private DataSource getDataSource(String workspaceName, String storeName) throws NamingException {
-        return getLayerDataStore(workspaceName, storeName).getDataSource();
-    }
-
-    private String getSchemaName(String workspaceName, String storeName) {
-        return getLayerDataStore(workspaceName, storeName).getDataStoreParameter("schema");
-    }
-
-    private LayerDataStore getLayerDataStore(String workspaceName, String storeName) {
-        return new LayerDataStore(getCatalog(), workspaceName, storeName);
     }
 }
