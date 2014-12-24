5556192fbcc58b5496f68541cb7035022b1393f2
Tommy Fotak <tommy.fotak@gmail.com>
2014-12-24 12:28:19 +1100

    Allow user to edit filter type
    Required to override the default type as determined by GeoServer, in
    particular handy for Geometry types

--- src/main/java/au/org/emii/geoserver/extensions/filters/LayerFilterForm.java
+++ src/main/java/au/org/emii/geoserver/extensions/filters/LayerFilterForm.java
@@ -10,10 +10,7 @@ package au.org.emii.geoserver.extensions.filters;
 import au.org.emii.geoserver.extensions.filters.layer.data.Filter;
 import au.org.emii.geoserver.extensions.filters.layer.data.FilterConfiguration;
 import au.org.emii.geoserver.extensions.filters.layer.data.io.FilterConfigurationFile;
-import au.org.emii.geoserver.extensions.filters.layer.data.io.FilterConfigurationIO;
-import au.org.emii.geoserver.extensions.filters.layer.data.io.FilterConfigurationWriter;
 import freemarker.template.TemplateException;
-import org.apache.commons.io.IOUtils;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.Form;
@@ -25,12 +22,8 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-import org.geotools.util.logging.Logging;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.logging.Logger;
 
 public class LayerFilterForm extends Form<FilterConfiguration> {
 
@@ -44,7 +37,17 @@ public class LayerFilterForm extends Form<FilterConfiguration> {
                 final Filter filter = item.getModelObject();
                 item.add(new CheckBox("propertyEnabled", new PropertyModel<Boolean>(filter, "enabled")));
                 item.add(new Label("propertyName", filter.getName()));
-                item.add(new Label("propertyType", filter.getType()));
+                item.add(new TextField<String>("propertyType", new Model<String>() {
+                    @Override
+                    public String getObject() {
+                        return filter.getType();
+                    }
+
+                    @Override
+                    public void setObject(final String value) {
+                        filter.setType(value);
+                    }
+                }));
                 item.add(new TextField<String>("propertyLabel", new Model<String>() {
                     @Override
                     public String getObject() {
