bcce01ba6583beba47449db8a6e60e54ed8752e5
julian1 <julian.atkinson71@gmail.com>
2015-04-14 17:28:41 +1000

    initial prototype

---
+++ src/test/java/au/org/emii/MyTest.java
@@ -0,0 +1,25 @@
+
+package au.org.emii;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class MyTest {
+
+    @Before
+    public void merge() {
+		System.out.println( "whoot UNIT test" );
+    }   
+
+
+    @Test
+    public void newListTest() {
+
+    }   
+
+}
