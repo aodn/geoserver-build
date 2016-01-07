636cc26446c005a1fea0df7b831c90bbf916c6dd
Dan Fruehauf <malkodan@gmail.com>
2016-01-07 12:39:46 +1100

    URL mangler test fix
    Do not source url mangling map from Main class, but assemble one
    that only the test can use. This avoids environment changes breaking
    the tests.

--- src/extension/wps/src/test/java/au/org/emii/gogoduck/worker/URLManglerTest.java
+++ src/extension/wps/src/test/java/au/org/emii/gogoduck/worker/URLManglerTest.java
@@ -12,13 +12,18 @@ import static org.junit.Assert.assertEquals;
 public class URLManglerTest {
     @Test
     public void testMangle() throws Exception {
-        URLMangler.setUrlManglingMap(Main.getUrlMangling());
+        Map<String, String> urlMangling = new HashMap<String, String>();
+        urlMangling.put("^/mnt/imos-t3/", "https://data.aodn.org.au/");
+        urlMangling.put("^/mnt/opendap/2/SRS/", "https://thredds.aodn.org.au/thredds/fileServer/IMOS/SRS/");
+        urlMangling.put("^IMOS/", "http://imos-data.aodn.org.au/IMOS/");
+
+        URLMangler.setUrlManglingMap(urlMangling);
 
         URL u;
         u = URLMangler.mangle(new URI("/mnt/imos-t3/file.nc"));
         assertEquals(u, new URL("https://data.aodn.org.au/file.nc"));
 
         u = URLMangler.mangle(new URI("/mnt/opendap/2/SRS/sst/ghrsst/L3S-1d/dn/2014/20141011092000-ABOM-L3S_GHRSST-SSTfnd-AVHRR_D-1d_dn-v02.0-fv02.0.nc"));
-        assertEquals(u, new URL("https://thredds.aodn.org.au/thredds/fileServer/srs/sst/ghrsst/L3S-1d/dn/2014/20141011092000-ABOM-L3S_GHRSST-SSTfnd-AVHRR_D-1d_dn-v02.0-fv02.0.nc"));
+        assertEquals(u, new URL("https://thredds.aodn.org.au/thredds/fileServer/IMOS/SRS/sst/ghrsst/L3S-1d/dn/2014/20141011092000-ABOM-L3S_GHRSST-SSTfnd-AVHRR_D-1d_dn-v02.0-fv02.0.nc"));
     }
 }
