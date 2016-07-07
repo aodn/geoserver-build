732dceb5c65212267fbaa129730aedcd746ccd3d
Craig Jones <Craig.Jones@utas.edu.au>
2016-07-08 08:03:22 +1000

    Systest fix for aodn/aodn-portal#2234 Remove remapped LONGITUDE/LATITUDE variables and remove SRS variable list - it doesn't apply to SRS_OC

--- src/extension/wps/src/main/java/au/org/emii/gogoduck/worker/GoGoDuckModule_srs_oc.java
+++ src/extension/wps/src/main/java/au/org/emii/gogoduck/worker/GoGoDuckModule_srs_oc.java
@@ -1,5 +1,8 @@
 package au.org.emii.gogoduck.worker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GoGoDuckModule_srs_oc extends GoGoDuckModule_srs {
     @Override
     public SubsetParameters getSubsetParameters() {
@@ -10,7 +13,17 @@ public class GoGoDuckModule_srs_oc extends GoGoDuckModule_srs {
         // Rename LONGITUDE -> longitude
         subsetParametersNew.put("latitude", subset.get("LATITUDE"));
         subsetParametersNew.put("longitude", subset.get("LONGITUDE"));
+        subsetParametersNew.remove("LATITUDE");
+        subsetParametersNew.remove("LONGITUDE");
 
         return subsetParametersNew;
+    }
+
+    @Override
+    public List<String> ncksExtraParameters() {
+        List<String> ncksExtraParameters = new ArrayList<String>();
+        ncksExtraParameters.add("--mk_rec_dmn");
+        ncksExtraParameters.add("time");
+        return ncksExtraParameters;
     }
 }
