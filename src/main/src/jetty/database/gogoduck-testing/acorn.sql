64a382ea4f0409a92c8f84db61439f34fab44a55
bhasin85 <bhasin85@gmail.com>
2016-07-19 10:10:29 +1000

    Cars and SRS Layers

--- src/main/src/jetty/database/gogoduck-testing/acorn.sql
+++ src/main/src/jetty/database/gogoduck-testing/acorn.sql
@@ -1,3 +1,3 @@
-INSERT INTO acorn_hourly_avg_nonqc_timeseries_url VALUES ('IMOS/ACORN/gridded_1h-avg-current-map_non-QC/CBG/2009/08/27/IMOS_ACORN_V_20090827T163000Z_CBG_FV00_1-hour-avg.nc','2009-08-27 16:30:00');
+INSERT INTO acorn_hourly_avg_nonqc_timeseries_url VALUES ('IMOS/ACORN/gridded_1h-avg-current-map_non-QC/ROT/2016/07/18/IMOS_ACORN_V_20160718T003000Z_ROT_FV00_1-hour-avg.nc','2016-07-18 00:30:00');
 INSERT INTO acorn_hourly_avg_nonqc_timeseries_url VALUES ('IMOS/ACORN/gridded_1h-avg-current-map_non-QC/ROT/2014/10/03/IMOS_ACORN_V_20141003T003000Z_ROT_FV00_1-hour-avg.nc','2014-10-03 00:30:00');
-INSERT INTO acorn_hourly_avg_nonqc_timeseries_url VALUES ('IMOS/ACORN/gridded_1h-avg-current-map_non-QC/TURQ/2012/10/18/IMOS_ACORN_V_20121018T010000Z_TURQ_FV00_1-hour-avg.nc','2012-10-18 01:00:00');
+INSERT INTO acorn_hourly_avg_nonqc_timeseries_url VALUES ('IMOS/ACORN/gridded_1h-avg-current-map_non-QC/ROT/2016/07/18/IMOS_ACORN_V_20160718T013000Z_ROT_FV00_1-hour-avg.nc','2016-07-18 01:30:00');
