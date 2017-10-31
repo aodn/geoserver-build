96c208f4d35cb58b037ce555a72e76f48de39297
pmbohm <pmbohm@utas.edu.au>
2017-10-31 12:03:59 +1100

    Date formatting for CSV with header output

--- src/extension/csv-with-metadata-header/src/main/java/au/org/emii/geoserver/wfs/response/CSVWithMetadataHeaderOutputFormat.java
+++ src/extension/csv-with-metadata-header/src/main/java/au/org/emii/geoserver/wfs/response/CSVWithMetadataHeaderOutputFormat.java
@@ -6,18 +6,6 @@
  */
 package au.org.emii.geoserver.wfs.response;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.SQLException;
-
-import java.util.logging.Logger;
-
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.FeatureTypeInfo;
@@ -26,42 +14,59 @@ import org.geoserver.platform.Operation;
 import org.geoserver.platform.ServiceException;
 import org.geoserver.wfs.WFSGetFeatureOutputFormat;
 import org.geoserver.wfs.request.FeatureCollectionResponse;
-import org.geoserver.wfs.response.CSVOutputFormat;
+import org.geoserver.wfs.request.GetFeatureRequest;
+import org.geoserver.wfs.request.Query;
 import org.geotools.data.Transaction;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.feature.type.DateUtil;
 import org.geotools.jdbc.JDBCDataStore;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
+
+import javax.xml.namespace.QName;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class CSVWithMetadataHeaderOutputFormat extends WFSGetFeatureOutputFormat {
+
+    static final Pattern CSV_ESCAPES = Pattern.compile("[\"\n,\r]");
 
     static Logger LOGGER = org.geotools.util.logging.Logging.getLogger("au.org.emii.geoserver.wfs.response");
 
     private Catalog catalog;
-    private CSVOutputFormat csvOutputFormat;
 
-    public CSVWithMetadataHeaderOutputFormat(GeoServer gs, Catalog catalog, CSVOutputFormat csvOutputFormat) {
+    public CSVWithMetadataHeaderOutputFormat(GeoServer gs, Catalog catalog) {
         super(gs, "csv-with-metadata-header");
         this.catalog = catalog;
-        this.csvOutputFormat = csvOutputFormat;
     }
 
-    @Override
-    public String getMimeType(Object value, Operation operation) {
-        return this.csvOutputFormat.getMimeType(value, operation);
+    public String getMimeType(Object value, Operation operation) throws ServiceException {
+        return "text/csv";
     }
 
-    @Override
-    public String getPreferredDisposition(Object value, Operation operation) {
-        return this.csvOutputFormat.getPreferredDisposition(value, operation);
-    }
-
-    @Override
-    public String getAttachmentFileName(Object value, Operation operation) {
-        return this.csvOutputFormat.getAttachmentFileName(value, operation);
-    }
-
-    @Override
     public String getCapabilitiesElementName() {
-        return this.csvOutputFormat.getCapabilitiesElementName();
+        return "CSV";
     }
 
     @Override
@@ -70,7 +75,89 @@ public class CSVWithMetadataHeaderOutputFormat extends WFSGetFeatureOutputFormat
                          Operation getFeature) throws IOException, ServiceException {
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(output));
         writeMetadata(featureCollection, getMetadataFeatureName(featureCollection), w);
-        this.csvOutputFormat.write(featureCollection, output, getFeature);
+        writeData(featureCollection, output, getFeature);
+    }
+
+
+    public void writeData(FeatureCollectionResponse featureCollection, OutputStream output, Operation getFeature) throws IOException, ServiceException {
+        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(output, this.gs.getGlobal().getSettings().getCharset()));
+        FeatureCollection<?, ?> fc = (FeatureCollection)featureCollection.getFeature().get(0);
+        Object att;
+
+        SimpleFeatureType ft = (SimpleFeatureType)((FeatureCollection)fc).getSchema();
+        w.write("FID,");
+
+        for(int i = 0; i < ft.getAttributeCount(); ++i) {
+            AttributeDescriptor ad = ft.getDescriptor(i);
+            w.write(this.prepCSVField(ad.getLocalName()));
+            if (i < ft.getAttributeCount() - 1) {
+                w.write(",");
+            }
+        }
+
+        w.write("\r\n");
+        NumberFormat coordFormatter = NumberFormat.getInstance(Locale.US);
+        coordFormatter.setMaximumFractionDigits(this.getInfo().getGeoServer().getSettings().getNumDecimals());
+        coordFormatter.setGroupingUsed(false);
+        FeatureIterator i = ((FeatureCollection)fc).features();
+
+        try {
+            while (i.hasNext()) {
+                Feature f = i.next();
+                w.write(this.prepCSVField(f.getIdentifier().getID()));
+                w.write(",");
+
+                for(int j = 0; j < ((SimpleFeature)f).getAttributeCount(); ++j) {
+                    att = ((SimpleFeature)f).getAttribute(j);
+                    if (att != null) {
+                        String value = formatToString(att, coordFormatter);
+                        w.write(this.prepCSVField(value));
+                    }
+
+                    if (j < ((SimpleFeature)f).getAttributeCount() - 1) {
+                        w.write(",");
+                    }
+                }
+                w.write("\r\n");
+            }
+        } finally {
+            i.close();
+        }
+        w.flush();
+    }
+
+    private String prepCSVField(String field) {
+        String mod = field.replaceAll("\"", "\"\"");
+        if (CSV_ESCAPES.matcher(mod).find()) {
+            mod = "\"" + mod + "\"";
+        }
+        return mod;
+    }
+
+    public String formatToString(Object att, NumberFormat coordFormatter) {
+        String value = null;
+
+        if (att instanceof Number) {
+            value = coordFormatter.format(att);
+        } else if (att instanceof Date) {
+            if (att instanceof java.sql.Date) {
+                value = DateUtil.serializeSqlDate((java.sql.Date)att);
+            } else if (att instanceof Time) {
+                value = DateUtil.serializeSqlTime((Time)att);
+            }else {
+                value = utcDateString((Date)att);
+            }
+        } else {
+            value = att.toString();
+        }
+        return value;
+    }
+
+    private String utcDateString(Date date) {
+        DateTime dt = new DateTime( date, DateTimeZone.UTC );
+
+        DateTimeFormatter fmt = ISODateTimeFormat.dateTimeNoMillis();
+        return fmt.print(dt);
     }
 
     private void writeMetadata(FeatureCollectionResponse featureCollection,
@@ -121,6 +208,7 @@ public class CSVWithMetadataHeaderOutputFormat extends WFSGetFeatureOutputFormat
         //     <featureCollection schema>_metadata_summary
         //
         String schema = getDataStoreForFeatureCollection(featureCollection).getDatabaseSchema();
+
         return "parameters_mapping." + schema + "_metadata_summary";
     }
 
@@ -128,5 +216,11 @@ public class CSVWithMetadataHeaderOutputFormat extends WFSGetFeatureOutputFormat
         dataStore.closeSafe(rs);
         dataStore.closeSafe(stmt);
         dataStore.closeSafe(con);
+    }
+
+    public String getAttachmentFileName(Object value, Operation operation) {
+        GetFeatureRequest request = GetFeatureRequest.adapt(operation.getParameters()[0]);
+        String outputFileName = ((QName)((Query)request.getQueries().get(0)).getTypeNames().get(0)).getLocalPart();
+        return outputFileName + ".csv";
     }
 }
