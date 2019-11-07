df06e936f1cbf32f2eaa40f699d08806064db1bb
Angus Scheibner <angus.scheibner@utas.edu.au>
2019-11-07 12:10:40 +1100

    Update bumpversion to use atomic git push

--- bumpversion.sh
+++ bumpversion.sh
@@ -24,8 +24,7 @@ _update_git() {
   git add pom.xml '*/pom.xml'
   git commit -m "Bump version to ${version}"
   git tag -a -f -m "Bump version to ${version}" ${version}
-  git push origin tag ${version}
-  git push origin HEAD:${RELEASE_BRANCH}
+  git push --atomic origin "HEAD:${RELEASE_BRANCH}" tag "${version}"
 }
 
 _bumpversion() {
