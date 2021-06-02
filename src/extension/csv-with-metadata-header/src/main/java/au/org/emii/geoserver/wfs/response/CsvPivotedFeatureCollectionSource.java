e83aefad681109d609d10d9c0be0974df391740d
Alen Paulic <alen.paulic@utas.edu.au>
2021-06-03 07:52:12 +1000

    Remove reliance of first row containing all columns

--- src/extension/csv-with-metadata-header/src/main/java/au/org/emii/geoserver/wfs/response/CsvPivotedFeatureCollectionSource.java
+++ src/extension/csv-with-metadata-header/src/main/java/au/org/emii/geoserver/wfs/response/CsvPivotedFeatureCollectionSource.java
@@ -1,9 +1,13 @@
 package au.org.emii.geoserver.wfs.response;
 
 import org.geoserver.platform.Operation;
+import org.geoserver.wfs.TypeInfoCollectionWrapper;
 import org.geoserver.wfs.request.GetFeatureRequest;
+import org.geotools.data.FeatureSource;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.feature.visitor.CalcResult;
+import org.geotools.feature.visitor.UniqueVisitor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -11,16 +15,21 @@ import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.feature.type.AttributeType;
+import org.postgresql.util.PGobject;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import static au.org.emii.geoserver.wfs.response.CSVWithMetadataHeaderOutputFormat.JSON_FIELD_TYPE;
+import static org.geotools.gce.imagemosaic.Utils.FF;
 
 
 public class CsvPivotedFeatureCollectionSource implements CsvSource {
 
+    private static final int MAX_PIVOT_COLUMNS = 10000;
+    private final int DEFAULT_VALUE = 0;
     private final SimpleFeatureCollection featureCollection;
     private final SimpleFeatureIterator featureIterator;
     private final ArrayList<String> nonPivotColumnNames;
@@ -30,10 +39,12 @@ public class CsvPivotedFeatureCollectionSource implements CsvSource {
     static Logger LOGGER = org.geotools.util.logging.Logging.getLogger("au.org.emii.geoserver.wfs.response");
 
     private SimpleFeature nextFeature;
+    private JSONObject pivotedObject;
 
     public CsvPivotedFeatureCollectionSource(Operation getFeatureOperation, SimpleFeatureCollection featureCollection) {
         this.featureCollection = featureCollection;
         this.featureIterator = featureCollection.features();
+        this.pivotedObject = new JSONObject();
 
         //  Get feature type definition
         
@@ -65,20 +76,43 @@ public class CsvPivotedFeatureCollectionSource implements CsvSource {
             nextFeature = featureIterator.next();
         }
 
-        // Load JSON field value and list attribute/column names, sorted alphabetically
+        // Loop through each source column of JSON objects and build up a list of distinct attribute names
+        // alphabetically to then be rendered into columns
 
-        this.pivotColumnNames = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        Set<String> foundColumnNames = new HashSet<>();
         for (String sourceColumnName : pivotSourceColumnNames) {
-            String column_json_string = (String) nextFeature.getAttribute(sourceColumnName);
-            JSONParser parser = new JSONParser();
             try {
-                JSONObject json = (JSONObject) parser.parse(column_json_string);
-                Set<String> keys = json.keySet();
-                this.pivotColumnNames.addAll(keys.stream().sorted().collect(Collectors.toList()));
-            } catch(ParseException pe) {
-                LOGGER.warning(pe.getMessage());
+                FeatureSource featureSource = ((TypeInfoCollectionWrapper.Simple) featureCollection)
+                        .getFeatureTypeInfo().getFeatureSource(null, null);
+                SimpleFeatureCollection features = (SimpleFeatureCollection) featureSource.getFeatures();
+
+                UniqueVisitor visitor = new UniqueVisitor(FF.property(sourceColumnName)) {
+                    @Override
+                    public boolean hasLimits() {
+                        // force usage of visitor limits, also for size extraction "query"
+                        return true;
+                    }
+                };
+
+                visitor.setMaxFeatures(MAX_PIVOT_COLUMNS);
+                features.accepts(visitor, null);
+                if (visitor.getResult() != null && visitor.getResult().toList() != null) {
+                    for (Object result : visitor.getResult().toList()) {
+                        try {
+                            JSONObject json = (JSONObject) parser.parse(((PGobject) result).getValue());
+                            foundColumnNames.addAll(json.keySet());
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
+
+        this.pivotColumnNames = foundColumnNames.stream().sorted().collect(Collectors.toList());
     }
 
     @Override
@@ -103,28 +137,26 @@ public class CsvPivotedFeatureCollectionSource implements CsvSource {
         }
 
         List<Object> result = new ArrayList<>();
-        Map<String, Object> currentGroup = new HashMap();
 
         // Add non-pivot attribute values to column values
         for (String columnName: nonPivotColumnNames) {
             result.add(nextFeature.getAttribute(columnName));
         }
 
-        // Add pivoted column values to column values
+        // Add pivoted attribute values to column values. If missing in row use default value
+
+        JSONParser parser = new JSONParser();
         for (String sourceColumnName : pivotSourceColumnNames) {
             String column_json_string = (String) nextFeature.getAttribute(sourceColumnName);
-            JSONParser parser = new JSONParser();
             try {
                 JSONObject json = (JSONObject) parser.parse(column_json_string);
-                Set<String> keys = json.keySet();
-                for (String key : keys.stream().sorted().collect(Collectors.toList())){
-                    result.add(json.get(key));
+                for (String key : this.pivotColumnNames.stream().sorted().collect(Collectors.toList())){
+                    result.add(json.getOrDefault(key, DEFAULT_VALUE));
                 }
             } catch(ParseException pe) {
                 LOGGER.warning(pe.getMessage());
             }
         }
-
 
         if (featureIterator.hasNext()) {
             nextFeature = featureIterator.next();
